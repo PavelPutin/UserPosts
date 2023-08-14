f2d27c53a849762cc66014329512cca49e2cff24
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-08-14 14:22:33 +0300

    Initial commit

---
+++ src/main/java/com/example/userposts/UserPostsApplication.java
@@ -0,0 +1,13 @@
+package com.example.userposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPostsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserPostsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userposts/UserPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPostsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
