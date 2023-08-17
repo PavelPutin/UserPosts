5400471c7cc0a7256b33538cc590b48a7ee396e7
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-08-17 16:41:06 +0300

    fix: migrage FriendsPK class to Lombok

--- src/main/java/com/example/userposts/model/FriendsPK.java
+++ src/main/java/com/example/userposts/model/FriendsPK.java
@@ -1,47 +1,16 @@
 package com.example.userposts.model;
 
-import java.io.Serializable;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FriendsPK implements Serializable {
     private User whoAdd;
     private User whoShouldAccept;
-
-    private FriendsPK() {
-    }
-
-    public FriendsPK(User whoAdd, User whoShouldAccept) {
-        this.whoAdd = whoAdd;
-        this.whoShouldAccept = whoShouldAccept;
-    }
-
-    public User getWhoAdd() {
-        return whoAdd;
-    }
-
-    public void setWhoAdd(User whoAdd) {
-        this.whoAdd = whoAdd;
-    }
-
-    public User getWhoShouldAccept() {
-        return whoShouldAccept;
-    }
-
-    public void setWhoShouldAccept(User whoShouldAccept) {
-        this.whoShouldAccept = whoShouldAccept;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FriendsPK friendsPK = (FriendsPK) o;
-        return Objects.equals(getWhoAdd(), friendsPK.getWhoAdd()) && Objects.equals(getWhoShouldAccept(), friendsPK.getWhoShouldAccept());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getWhoAdd(), getWhoShouldAccept());
-    }
 }
 
---
+++ src/main/java/com/example/userposts/model/PostTag.java
@@ -0,0 +1,2 @@
+package com.example.userposts.model;public class PostTag {
+}
